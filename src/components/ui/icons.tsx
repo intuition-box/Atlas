85bc5dc9136d70eb9d3ab7566c84183de1891a9f
Saulo <22698083+saulodigital@users.noreply.github.com>
2026-01-18 01:30:19 +0000

    ui icons

---
+++ src/components/ui/icons.tsx
@@ -0,0 +1,62 @@
+import { forwardRef, memo, ReactNode, SVGProps } from 'react';
+
+/**
+ * Icons base
+ */
+
+type IconBaseProps = Omit<SVGProps<SVGSVGElement>, 'children'> & {
+  viewBox?: string;
+  children?: ReactNode;
+  width?: number | string;
+  height?: number | string;
+  size?: number | string;
+};
+
+const defaultAttrs = {
+  fill: 'currentColor',
+  stroke: 'currentColor',
+  strokeWidth: 0 as number,
+  strokeLinecap: 'round' as const,
+  strokeLinejoin: 'round' as const,
+};
+
+const IconBase = forwardRef<SVGSVGElement, IconBaseProps>(
+  ({
+    viewBox = '0 0 24 24',
+    width = "1lh",
+    height = "1lh",
+    size,
+    children,
+    ...props
+  }, ref) => (
+    <svg
+      ref={ref}
+      viewBox={viewBox}
+      width={size ?? width}
+      height={size ?? height}
+      role={props['aria-label'] ? 'img' : 'presentation'}
+      aria-hidden={props['aria-label'] ? 'false' : 'true'}
+      {...defaultAttrs}
+      {...props}
+    >
+      {children}
+    </svg>
+  )
+);
+
+/**
+ * Icons
+ */
+
+export const UserIcon = memo(forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElement>>((props, ref) => (
+  <IconBase ref={ref} aria-label="Users icon" {...props}>
+    <path d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" />
+  </IconBase>
+)));
+
+export const UsersIcon = memo(forwardRef<SVGSVGElement, React.SVGProps<SVGSVGElement>>((props, ref) => (
+  <IconBase ref={ref} aria-label="Users icon" {...props}>
+    <path d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z" />
+    <path d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
+  </IconBase>
+)));
