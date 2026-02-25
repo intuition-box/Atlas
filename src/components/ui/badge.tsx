193865c1e075d917c30c1ec446dc284e75e1b764
Saulo <22698083+saulodigital@users.noreply.github.com>
2026-02-25 04:41:52 +0000

    badge/button variants

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -5,17 +5,19 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const badgeVariants = cva(
-  "h-5 gap-1 rounded-4xl border border-transparent px-2 py-0.5 text-xs font-medium transition-all has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&>svg]:size-3! inline-flex items-center justify-center w-fit whitespace-nowrap shrink-0 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-colors overflow-hidden group/badge",
+  "h-5 gap-1 rounded-4xl border px-2 py-0.5 text-xs font-medium transition-all has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&>svg]:size-3! inline-flex items-center justify-center w-fit whitespace-nowrap shrink-0 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-colors overflow-hidden group/badge",
   {
     variants: {
       variant: {
-        default: "bg-primary/10 text-primary [a]:hover:bg-primary/15",
-        solid: "bg-primary text-primary-foreground [a]:hover:bg-primary/80",
-        secondary: "bg-secondary text-secondary-foreground [a]:hover:bg-secondary/80",
-        destructive: "bg-destructive/10 [a]:hover:bg-destructive/20 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 text-destructive dark:bg-destructive/20",
-        outline: "border-border text-foreground [a]:hover:bg-muted [a]:hover:text-muted-foreground bg-input/30",
-        ghost: "hover:bg-muted hover:text-muted-foreground dark:hover:bg-muted/50",
-        link: "text-primary underline-offset-4 hover:underline",
+        default: "bg-input/30 text-primary border-primary/20 [a]:hover:bg-input/50",
+        solid: "bg-primary text-primary-foreground border-transparent [a]:hover:bg-primary/80",
+        secondary: "bg-input/30 text-secondary-foreground border-border [a]:hover:bg-input/50",
+        positive: "bg-input/30 text-emerald-500 border-emerald-500/20 [a]:hover:bg-input/50",
+        info: "bg-input/30 text-amber-600 border-amber-500/20 [a]:hover:bg-input/50",
+        destructive: "bg-input/30 text-destructive border-destructive/20 [a]:hover:bg-input/50 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40",
+        outline: "bg-input/30 text-foreground border-border [a]:hover:bg-input/50",
+        ghost: "border-transparent hover:bg-muted hover:text-muted-foreground dark:hover:bg-muted/50",
+        link: "border-transparent text-primary underline-offset-4 hover:underline",
       },
     },
     defaultVariants: {
