f511578dcdefe6e94f9deee6cbc08767ad8cc56a
Saulo <22698083+saulodigital@users.noreply.github.com>
2026-03-12 19:34:37 +0000

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Providers } from "@/components/providers";
 import { NavigationController } from "@/components/navigation/navigation-controller";
 
 import "./globals.css";
+import { meta } from "@/config/constants";
 
 const sans = localFont({
   src: "../fonts/Supreme-Variable.woff2",
@@ -22,26 +23,62 @@ const mono = IBM_Plex_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Atlas",
-  description: "",
-  icons: [{
-    rel: "icon",
-    url: "/favicon.ico"
-  }],
+  metadataBase: new URL(meta.url),
+  title: {
+    default: `${meta.name} | ${meta.title}`,
+    template: `%s | ${meta.name}`,
+  },
+  description: meta.description,
+  alternates: {
+    canonical: '/',
+  },
   openGraph: {
-    title: "Atlas",
-    description: "",
-    url: "",
-    siteName: "Atlas",
-    images: [],
-    type: "website",
+    title: meta.title,
+    description: meta.description,
+    url: meta.url,
+    siteName: meta.name,
+    images: [
+      {
+        url: meta.og.image,
+        width: meta.og.width,
+        height: meta.og.height,
+      },
+    ],
+    locale: meta.og.locale,
+    type: meta.og.type,
+  },
+  robots: {
+    index: true,
+    follow: true,
+    noarchive: true,
+    nosnippet: false,
+    noimageindex: true,
+    nocache: true,
+  },
+  icons: {
+    icon: {
+      rel: 'icon',
+      url: meta.icons.favicon,
+      type: 'image/x-icon',
+      sizes: '48x48',
+    },
+    shortcut: {
+      rel: 'icon',
+      url: meta.icons.app,
+      type: 'image/svg+xml',
+    },
+    apple: meta.icons.touchIcon,
+    other: {
+      rel: 'image_src',
+      url: meta.og.image,
+    },
   },
 };
 
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
-  themeColor: "#000000",
+  themeColor: meta.themeColor,
   colorScheme: "dark",
 };
 
